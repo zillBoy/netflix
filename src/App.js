e03e17c74358a24adf34a23f64bda1268c593925
Muhammad Usman <prousman741@gmail.com>
2021-10-06 22:47:52 +0500

    Jumbotron - implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Jumbotron from './components/jumbotron'
+import jumboData from './fixtures/jumbo.json'
+
 export default function App() {
   return (
-    <p>Helo</p>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
